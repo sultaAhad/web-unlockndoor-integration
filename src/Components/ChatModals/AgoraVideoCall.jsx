e028083d594f2f5cbd77c47fa5b9470f05fa5c54
surbzia <101662816+surbzia@users.noreply.github.com>
2025-09-15 22:05:12 +0500

    chanegs made

--- src/Components/ChatModals/AgoraVideoCall.jsx
+++ src/Components/ChatModals/AgoraVideoCall.jsx
@@ -208,8 +208,8 @@ const AgoraVideoCall = ({ onCall, onCallEnd }) => {
   }, [users]);
 
   return (
-		<div className="chat_img_wrapper">
-			{/* {callLink && (
+    <div className="chat_img_wrapper">
+      {/* {callLink && (
         <div className="call-link">
           <p>Share this link to invite someone:</p>
           <input
@@ -228,115 +228,115 @@ const AgoraVideoCall = ({ onCall, onCallEnd }) => {
           </button>
         </div>
       )} */}
-			<figure className="position-relative">
-				<div className="video-call-layout">
-					<div className="remote-video-large">
-						<div id="remote-video" className="video-player"></div>
-						{users.length === 0 && (
-							<div className="calling-overlay">
-								{videoCallData?.data?.type == "isCalling" && (
-									<h2 className="text-white secondary-medium-font">
-										Calling...
-									</h2>
-								)}
-								{videoCallData?.data?.type == "IsReceiving" && (
-									<h2 className="text-white secondary-medium-font">
-										Connecting Call...
-									</h2>
-								)}
-							</div>
-						)}
-					</div>
-					<div className="local-video-small">
-						<div id="local-video" className="video-player"></div>
-						<div className="user-info">You ({user?.name})</div>
-					</div>
-				</div>
-				<div className="row">
-					<div className="col-md-12 d-flex justify-content-center pt-3">
-						<div className="chat_wrap mx-2 text-center">
-							<Button
-								type="button"
-								onClick={toggleVideo}
-								className="bg-transparent border-0 p-0"
-							>
-								<span className="chat_span">
-									<i
-										className={`fa-solid   ${
-											isVideoEnabled
-												? "fa-video text-dark"
-												: "fa-video-slash text-secondary"
-										}  fs-4`}
-									></i>
-								</span>
-								<div className="chat_desc mt-2">
-									<p className="level-8 secondary-medium-font text-dark">
-										{isVideoEnabled ? "Off Video" : "On Video"}
-									</p>
-								</div>
-							</Button>
-						</div>
-						<div className="chat_wrap mx-2 text-center">
-							<Button
-								type="button"
-								className="bg-transparent border-0 p-0"
-								onClick={toggleAudio}
-							>
-								<span className="chat_span">
-									<i
-										className={`fa-solid   ${
-											isAudioEnabled
-												? "fa-microphone text-dark"
-												: "fa-microphone-slash text-secondary"
-										} fs-4`}
-									></i>
-								</span>
-								<div className="chat_desc mt-2">
-									<p className="level-8 secondary-medium-font text-dark">
-										{isAudioEnabled ? "Mute" : "Unmute"}
-									</p>
-								</div>
-							</Button>
-						</div>
-						<div className="chat_wrap mx-2 text-center">
-							<Button
-								type="button"
-								className="bg-transparent border-0 p-0"
-								onClick={leaveChannel}
-							>
-								<span className="chat_span chat_end">
-									{/* End call icon */}
-									<svg width="46" height="46" viewBox="0 0 46 46" fill="none">
-										<g clipPath="url(#clip0_1_795)">
-											<path
-												d="M7.39208 22.0101C9.35627 20.0459 11.7249 18.5257 14.498 17.4497C17.2712 16.3736 20.2294 15.8353 23.3727 15.8347C26.516 15.834 29.4702 16.3683 32.2351 17.4374C35 18.5066 37.3728 20.0308 39.3533 22.0101L40.3433 23L34.7807 28.5626L29.171 24.6971L29.1474 19.2995C28.1889 19.0323 27.2225 18.8359 26.2483 18.7102C25.274 18.5845 24.3077 18.5295 23.3491 18.5452C22.4377 18.5452 21.5025 18.6081 20.5433 18.7338C19.5842 18.8595 18.6103 19.0559 17.6216 19.323V24.7913L11.9411 28.539L6.40214 23L7.39208 22.0101Z"
-												fill="white"
-											/>
-										</g>
-										<defs>
-											<clipPath id="clip0_1_795">
-												<rect
-													width="32"
-													height="32"
-													fill="white"
-													transform="translate(46 23) rotate(135)"
-												/>
-											</clipPath>
-										</defs>
-									</svg>
-								</span>
-								<div className="chat_desc mt-2">
-									<p className="level-8 secondary-medium-font text-dark">
-										{"End Call"}
-									</p>
-								</div>
-							</Button>
-						</div>
-					</div>
-				</div>
-			</figure>
-		</div>
-	);
+      <figure className="position-relative">
+        <div className="video-call-layout">
+          <div className="remote-video-large">
+            <div id="remote-video" className="video-player"></div>
+            {users.length === 0 && (
+              <div className="calling-overlay">
+                {videoCallData?.data?.type == "isCalling" && (
+                  <h2 className="text-white secondary-medium-font">
+                    Calling...
+                  </h2>
+                )}
+                {videoCallData?.data?.type == "IsReceiving" && (
+                  <h2 className="text-white secondary-medium-font">
+                    Connecting Call...
+                  </h2>
+                )}
+              </div>
+            )}
+          </div>
+          <div className="local-video-small">
+            <div id="local-video" className="video-player"></div>
+            {/* <div className="user-info">You ({user?.name})</div> */}
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12 d-flex justify-content-center pt-3">
+            <div className="chat_wrap mx-2 text-center">
+              <Button
+                type="button"
+                onClick={toggleVideo}
+                className="bg-transparent border-0 p-0"
+              >
+                <span className="chat_span">
+                  <i
+                    className={`fa-solid   ${
+                      isVideoEnabled
+                        ? "fa-video text-dark"
+                        : "fa-video-slash text-secondary"
+                    }  fs-4`}
+                  ></i>
+                </span>
+                <div className="chat_desc mt-2">
+                  <p className="level-8 secondary-medium-font ">
+                    {isVideoEnabled ? "Off Video" : "On Video"}
+                  </p>
+                </div>
+              </Button>
+            </div>
+            <div className="chat_wrap mx-2 text-center">
+              <Button
+                type="button"
+                className="bg-transparent border-0 p-0"
+                onClick={toggleAudio}
+              >
+                <span className="chat_span">
+                  <i
+                    className={`fa-solid   ${
+                      isAudioEnabled
+                        ? "fa-microphone text-dark"
+                        : "fa-microphone-slash text-secondary"
+                    } fs-4`}
+                  ></i>
+                </span>
+                <div className="chat_desc mt-2">
+                  <p className="level-8 secondary-medium-font ">
+                    {isAudioEnabled ? "Mute" : "Unmute"}
+                  </p>
+                </div>
+              </Button>
+            </div>
+            <div className="chat_wrap mx-2 text-center">
+              <Button
+                type="button"
+                className="bg-transparent border-0 p-0"
+                onClick={leaveChannel}
+              >
+                <span className="chat_span chat_end">
+                  {/* End call icon */}
+                  <svg width="46" height="46" viewBox="0 0 46 46" fill="none">
+                    <g clipPath="url(#clip0_1_795)">
+                      <path
+                        d="M7.39208 22.0101C9.35627 20.0459 11.7249 18.5257 14.498 17.4497C17.2712 16.3736 20.2294 15.8353 23.3727 15.8347C26.516 15.834 29.4702 16.3683 32.2351 17.4374C35 18.5066 37.3728 20.0308 39.3533 22.0101L40.3433 23L34.7807 28.5626L29.171 24.6971L29.1474 19.2995C28.1889 19.0323 27.2225 18.8359 26.2483 18.7102C25.274 18.5845 24.3077 18.5295 23.3491 18.5452C22.4377 18.5452 21.5025 18.6081 20.5433 18.7338C19.5842 18.8595 18.6103 19.0559 17.6216 19.323V24.7913L11.9411 28.539L6.40214 23L7.39208 22.0101Z"
+                        fill="white"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_1_795">
+                        <rect
+                          width="32"
+                          height="32"
+                          fill="white"
+                          transform="translate(46 23) rotate(135)"
+                        />
+                      </clipPath>
+                    </defs>
+                  </svg>
+                </span>
+                <div className="chat_desc mt-2">
+                  <p className="level-8 secondary-medium-font ">
+                    {"End Call"}
+                  </p>
+                </div>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </figure>
+    </div>
+  );
 };
 
 export default AgoraVideoCall;
