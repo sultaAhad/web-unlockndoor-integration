35ca1aa69dd5271d3309450544b1dcd0a6208ad9
Max Brian <maxbrain64@gmail.com>
2025-11-12 22:36:28 +0500

    changes made

--- src/Components/ProfileNavbar.jsx
+++ src/Components/ProfileNavbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, useLocation } from "react-router-dom";
 
 function ProfileNavbar() {
-	const location = useLocation();
+  const location = useLocation();
+  const { user } = useSelector((state) => state.auth);
 
-	// Function to check if the link is active
-	const isActive = (path) => {
+  const isActive = (path) => {
     if (path === "/woman/like-match") {
       return location.pathname === "/woman/like-match";
     }
@@ -13,30 +14,38 @@ function ProfileNavbar() {
   };
 
   const menuItems = [
-    { label: "My Profile", to: "/women-profiles" },
-    { label: "Dates Tab", to: "/dates-tab" },
-    { label: "Like/Match", to: "/woman/like-match" },
-    { label: "My Membership", to: "/my-membership-women" },
+    { show: true, label: "My Profile", to: "/women-profiles" },
+    {
+      show: user?.package?.slug == "platinum-package",
+      label: "Dates Tab",
+      to: "/dates-tab",
+    },
+    { show: true, label: "Like/Match", to: "/woman/like-match" },
+    { show: true, label: "My Membership", to: "/my-membership-women" },
   ];
 
-	return (
-		<div className="horizontal-navbar ms-2 me-2 mb-4">
-			{menuItems.map((item) => (
-				<span key={item.to} className="menu-item">
-					<Link
-						to={item.to}
-						className={
-							isActive(item.to)
-								? "active-link secondary-medium-font level-7"
-								: "inactive-link secondary-medium-font level-7"
-						}
-					>
-						{item.label}
-					</Link>
-				</span>
-			))}
-		</div>
-	);
+  return (
+    <div className="horizontal-navbar ms-2 me-2 mb-4">
+      {menuItems.map((item) => (
+        <>
+          {item.show && (
+            <span key={item.to} className="menu-item">
+              <Link
+                to={item.to}
+                className={
+                  isActive(item.to)
+                    ? "active-link secondary-medium-font level-7"
+                    : "inactive-link secondary-medium-font level-7"
+                }
+              >
+                {item.label}
+              </Link>
+            </span>
+          )}
+        </>
+      ))}
+    </div>
+  );
 }
 
 export default ProfileNavbar;
